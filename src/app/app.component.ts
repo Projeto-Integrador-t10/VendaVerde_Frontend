e19a6550aa1c91ffef0b780a108f9b003340241a
Diana Coelho <coelhodiana04@gmail.com>
2021-01-09 15:17:54 -0300

    add title no app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from './service/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'vendaverde';
   constructor(
     public auth: AuthService
    
